cc29356828898deef5afebc36d899baf4e0127fc
TopRichard <121792457+TopRichard@users.noreply.github.com>
2023-02-01 12:35:31 +0100

    Update EESSI-pilot-install-software.sh

--- EESSI-pilot-install-software.sh
+++ EESSI-pilot-install-software.sh
@@ -344,7 +344,7 @@ if [ ! "${EESSI_CPU_FAMILY}" = "ppc64le" ]; then
     check_exit_code $? "${ok_msg}" "${fail_msg}"
 fi
 
-<<<<<<< HEAD
+
 #echo ">> Installing RStudio-Server 1.3.1093..."
 #ok_msg="RStudio-Server installed, enjoy!"
 #fail_msg="Installation of RStudio-Server failed, might be OS deps..."
@@ -362,42 +362,6 @@ fi
 #fail_msg="Installation of Spark failed, no fireworks this time..."
 #$EB Spark-3.1.1-foss-2020a-Python-3.8.2.eb -r
 #check_exit_code $? "${ok_msg}" "${fail_msg}"
-
-#echo ">> Installing IPython 7.15.0..."
-#ok_msg="IPython installed, launch your Jupyter Notebooks!"
-#fail_msg="Installation of IPython failed, that's unexpected..."
-#$EB IPython-7.15.0-foss-2020a-Python-3.8.2.eb -r
-#check_exit_code $? "${ok_msg}" "${fail_msg}"
-
-#echo ">> Installing WRF 3.9.1.1..."
-#ok_msg="WRF installed, it's getting hot in here!"
-#fail_msg="Installation of WRF failed, that's unexpected..."
-#OMPI_MCA_pml=ucx UCX_TLS=tcp $EB WRF-3.9.1.1-foss-2020a-dmpar.eb -r --include-easyblocks-from-pr 2648
-#check_exit_code $? "${ok_msg}" "${fail_msg}"
-
-#echo ">> Installing Nextflow 22.10.1..."
-#ok_msg="Nextflow installed, the work must flow..."
-#fail_msg="Installation of Nextflow failed, that's unexpected..."
-#$EB -r --from-pr 16531 Nextflow-22.10.1.eb
-#check_exit_code $? "${ok_msg}" "${fail_msg}"
-=======
-echo ">> Installing RStudio-Server 1.3.1093..."
-ok_msg="RStudio-Server installed, enjoy!"
-fail_msg="Installation of RStudio-Server failed, might be OS deps..."
-$EB RStudio-Server-1.3.1093-foss-2020a-Java-11-R-4.0.0.eb --robot
-check_exit_code $? "${ok_msg}" "${fail_msg}"
-
-echo ">> Installing OSU-Micro-Benchmarks 5.6.3..."
-ok_msg="OSU-Micro-Benchmarks installed, yihaa!"
-fail_msg="Installation of OSU-Micro-Benchmarks failed, that's unexpected..."
-$EB OSU-Micro-Benchmarks-5.6.3-gompi-2020a.eb -r
-check_exit_code $? "${ok_msg}" "${fail_msg}"
-
-echo ">> Installing Spark 3.1.1..."
-ok_msg="Spark installed, set off the fireworks!"
-fail_msg="Installation of Spark failed, no fireworks this time..."
-$EB Spark-3.1.1-foss-2020a-Python-3.8.2.eb -r
-check_exit_code $? "${ok_msg}" "${fail_msg}"
 
 echo ">> Installing IPython 7.15.0..."
 ok_msg="IPython installed, launch your Jupyter Notebooks!"
