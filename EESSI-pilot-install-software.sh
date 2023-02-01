fc270e1458c6746a61770446801b4d75d0c19d8e
Cloud User <centos@vm-richard.novalocal>
2023-02-01 10:51:19 +0000

    modified the two files to add AdminX tool

--- EESSI-pilot-install-software.sh
+++ EESSI-pilot-install-software.sh
@@ -236,32 +236,32 @@ fi
 $EB $openblas_include_easyblocks_from_pr OpenBLAS-0.3.9-GCC-9.3.0.eb --robot
 check_exit_code $? "${ok_msg}" "${fail_msg}"
 
-echo ">> Installing OpenMPI..."
-ok_msg="OpenMPI installed, w00!"
-fail_msg="Installation of OpenMPI failed, that's not good..."
-$EB OpenMPI-4.0.3-GCC-9.3.0.eb --robot
-check_exit_code $? "${ok_msg}" "${fail_msg}"
+#echo ">> Installing OpenMPI..."
+#ok_msg="OpenMPI installed, w00!"
+#fail_msg="Installation of OpenMPI failed, that's not good..."
+#$EB OpenMPI-4.0.3-GCC-9.3.0.eb --robot
+#check_exit_code $? "${ok_msg}" "${fail_msg}"
 
-# install Python
-echo ">> Install Python 2.7.18 and Python 3.8.2..."
-ok_msg="Python 2.7.18 and 3.8.2 installed, yaay!"
-fail_msg="Installation of Python failed, oh no..."
-$EB Python-2.7.18-GCCcore-9.3.0.eb Python-3.8.2-GCCcore-9.3.0.eb --robot
-check_exit_code $? "${ok_msg}" "${fail_msg}"
+## install Python
+#echo ">> Install Python 2.7.18 and Python 3.8.2..."
+#ok_msg="Python 2.7.18 and 3.8.2 installed, yaay!"
+#fail_msg="Installation of Python failed, oh no..."
+#$EB Python-2.7.18-GCCcore-9.3.0.eb Python-3.8.2-GCCcore-9.3.0.eb --robot
+#check_exit_code $? "${ok_msg}" "${fail_msg}"
 
-echo ">> Installing Perl..."
-ok_msg="Perl installed, making progress..."
-fail_msg="Installation of Perl failed, this never happens..."
+#echo ">> Installing Perl..."
+#ok_msg="Perl installed, making progress..."
+#fail_msg="Installation of Perl failed, this never happens..."
 # use enhanced Perl easyblock from https://github.com/easybuilders/easybuild-easyblocks/pull/2640
 # to avoid trouble when using long installation prefix (for example with EESSI pilot 2021.12 on skylake_avx512...)
-$EB Perl-5.30.2-GCCcore-9.3.0.eb --robot --include-easyblocks-from-pr 2640
-check_exit_code $? "${ok_msg}" "${fail_msg}"
+#$EB Perl-5.30.2-GCCcore-9.3.0.eb --robot --include-easyblocks-from-pr 2640
+#check_exit_code $? "${ok_msg}" "${fail_msg}"
 
-echo ">> Installing Qt5..."
-ok_msg="Qt5 installed, phieuw, that was a big one!"
-fail_msg="Installation of Qt5 failed, that's frustrating..."
-$EB Qt5-5.14.1-GCCcore-9.3.0.eb --robot
-check_exit_code $? "${ok_msg}" "${fail_msg}"
+#echo ">> Installing Qt5..."
+#ok_msg="Qt5 installed, phieuw, that was a big one!"
+#fail_msg="Installation of Qt5 failed, that's frustrating..."
+#$EB Qt5-5.14.1-GCCcore-9.3.0.eb --robot
+#check_exit_code $? "${ok_msg}" "${fail_msg}"
 
 # skip test step when installing SciPy-bundle on aarch64,
 # to dance around problem with broken numpy tests;
@@ -311,17 +311,17 @@ fail_msg="Installation of Bioconductor failed, that's annoying..."
 $EB R-bundle-Bioconductor-3.11-foss-2020a-R-4.0.0.eb --robot
 check_exit_code $? "${ok_msg}" "${fail_msg}"
 
-echo ">> Installing TensorFlow 2.3.1..."
-ok_msg="TensorFlow 2.3.1 installed, w00!"
-fail_msg="Installation of TensorFlow failed, why am I not surprised..."
-$EB TensorFlow-2.3.1-foss-2020a-Python-3.8.2.eb --robot --include-easyblocks-from-pr 2218
-check_exit_code $? "${ok_msg}" "${fail_msg}"
+#echo ">> Installing TensorFlow 2.3.1..."
+#ok_msg="TensorFlow 2.3.1 installed, w00!"
+#fail_msg="Installation of TensorFlow failed, why am I not surprised..."
+#$EB TensorFlow-2.3.1-foss-2020a-Python-3.8.2.eb --robot --include-easyblocks-from-pr 2218
+#check_exit_code $? "${ok_msg}" "${fail_msg}"
 
-echo ">> Installing Horovod 0.21.3..."
-ok_msg="Horovod installed! Go do some parallel training!"
-fail_msg="Horovod installation failed. There comes the headache..."
-$EB Horovod-0.21.3-foss-2020a-TensorFlow-2.3.1-Python-3.8.2.eb --robot
-check_exit_code $? "${ok_msg}" "${fail_msg}"
+#echo ">> Installing Horovod 0.21.3..."
+#ok_msg="Horovod installed! Go do some parallel training!"
+#fail_msg="Horovod installation failed. There comes the headache..."
+#$EB Horovod-0.21.3-foss-2020a-TensorFlow-2.3.1-Python-3.8.2.eb --robot
+#check_exit_code $? "${ok_msg}" "${fail_msg}"
 
 if [ ! "${EESSI_CPU_FAMILY}" = "ppc64le" ]; then
 
@@ -332,41 +332,41 @@ if [ ! "${EESSI_CPU_FAMILY}" = "ppc64le" ]; then
     check_exit_code $? "${ok_msg}" "${fail_msg}"
 fi
 
-echo ">> Installing RStudio-Server 1.3.1093..."
-ok_msg="RStudio-Server installed, enjoy!"
-fail_msg="Installation of RStudio-Server failed, might be OS deps..."
-$EB RStudio-Server-1.3.1093-foss-2020a-Java-11-R-4.0.0.eb --robot
-check_exit_code $? "${ok_msg}" "${fail_msg}"
+#echo ">> Installing RStudio-Server 1.3.1093..."
+#ok_msg="RStudio-Server installed, enjoy!"
+#fail_msg="Installation of RStudio-Server failed, might be OS deps..."
+#$EB RStudio-Server-1.3.1093-foss-2020a-Java-11-R-4.0.0.eb --robot
+#check_exit_code $? "${ok_msg}" "${fail_msg}"
 
-echo ">> Installing OSU-Micro-Benchmarks 5.6.3..."
-ok_msg="OSU-Micro-Benchmarks installed, yihaa!"
-fail_msg="Installation of OSU-Micro-Benchmarks failed, that's unexpected..."
-$EB OSU-Micro-Benchmarks-5.6.3-gompi-2020a.eb -r
-check_exit_code $? "${ok_msg}" "${fail_msg}"
+#echo ">> Installing OSU-Micro-Benchmarks 5.6.3..."
+#ok_msg="OSU-Micro-Benchmarks installed, yihaa!"
+#fail_msg="Installation of OSU-Micro-Benchmarks failed, that's unexpected..."
+#$EB OSU-Micro-Benchmarks-5.6.3-gompi-2020a.eb -r
+#check_exit_code $? "${ok_msg}" "${fail_msg}"
 
-echo ">> Installing Spark 3.1.1..."
-ok_msg="Spark installed, set off the fireworks!"
-fail_msg="Installation of Spark failed, no fireworks this time..."
-$EB Spark-3.1.1-foss-2020a-Python-3.8.2.eb -r
-check_exit_code $? "${ok_msg}" "${fail_msg}"
+#echo ">> Installing Spark 3.1.1..."
+#ok_msg="Spark installed, set off the fireworks!"
+#fail_msg="Installation of Spark failed, no fireworks this time..."
+#$EB Spark-3.1.1-foss-2020a-Python-3.8.2.eb -r
+#check_exit_code $? "${ok_msg}" "${fail_msg}"
 
-echo ">> Installing IPython 7.15.0..."
-ok_msg="IPython installed, launch your Jupyter Notebooks!"
-fail_msg="Installation of IPython failed, that's unexpected..."
-$EB IPython-7.15.0-foss-2020a-Python-3.8.2.eb -r
-check_exit_code $? "${ok_msg}" "${fail_msg}"
+#echo ">> Installing IPython 7.15.0..."
+#ok_msg="IPython installed, launch your Jupyter Notebooks!"
+#fail_msg="Installation of IPython failed, that's unexpected..."
+#$EB IPython-7.15.0-foss-2020a-Python-3.8.2.eb -r
+#check_exit_code $? "${ok_msg}" "${fail_msg}"
 
-echo ">> Installing WRF 3.9.1.1..."
-ok_msg="WRF installed, it's getting hot in here!"
-fail_msg="Installation of WRF failed, that's unexpected..."
-OMPI_MCA_pml=ucx UCX_TLS=tcp $EB WRF-3.9.1.1-foss-2020a-dmpar.eb -r --include-easyblocks-from-pr 2648
-check_exit_code $? "${ok_msg}" "${fail_msg}"
+#echo ">> Installing WRF 3.9.1.1..."
+#ok_msg="WRF installed, it's getting hot in here!"
+#fail_msg="Installation of WRF failed, that's unexpected..."
+#OMPI_MCA_pml=ucx UCX_TLS=tcp $EB WRF-3.9.1.1-foss-2020a-dmpar.eb -r --include-easyblocks-from-pr 2648
+#check_exit_code $? "${ok_msg}" "${fail_msg}"
 
-echo ">> Installing Nextflow 22.10.1..."
-ok_msg="Nextflow installed, the work must flow..."
-fail_msg="Installation of Nextflow failed, that's unexpected..."
-$EB -r --from-pr 16531 Nextflow-22.10.1.eb
-check_exit_code $? "${ok_msg}" "${fail_msg}"
+#echo ">> Installing Nextflow 22.10.1..."
+#ok_msg="Nextflow installed, the work must flow..."
+#fail_msg="Installation of Nextflow failed, that's unexpected..."
+#$EB -r --from-pr 16531 Nextflow-22.10.1.eb
+#check_exit_code $? "${ok_msg}" "${fail_msg}"
 
 echo ">> Installing OSU-Micro-Benchmarks/5.7.1-gompi-2021a..."
 ok_msg="OSU-Micro-Benchmarks installed, yihaa!"
@@ -416,6 +416,15 @@ $EB SciPy-bundle-2021.05-foss-2021a.eb --robot
 check_exit_code $? "${ok_msg}" "${fail_msg}"
 
 ### add packages here
+
+export AdmixTools="AdmixTools-7.0.1-GCC-9.3.0.eb"
+echo ">> Installing ${AdmixTools}..."
+ok_msg="${AdmixTools} installed, let's solve some problems!"
+fail_msg="Installation of ${AdmixTools} failed, that's a pity..."
+$EB ${AdmixTools} --robot
+check_exit_code $? "${ok_msg}" "${fail_msg}"
+
+
 
 echo ">> Creating/updating Lmod cache..."
 export LMOD_RC="${EASYBUILD_INSTALLPATH}/.lmod/lmodrc.lua"
